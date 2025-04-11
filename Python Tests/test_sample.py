9c20331bcbc4e0e69e1b25c6827657a566ecbb66
anonymous298 <tackletalha@gmail.com>
2025-04-11 11:47:26 +0500

    python test

---
+++ Python Tests/test_sample.py
@@ -0,0 +1,13 @@
+def func(x):
+    '''
+    This function will take a number x and returns sum by 1.
+    '''
+
+    return x + 1
+
+def test_answer():
+    '''
+    This function will test  the function.
+    '''
+
+    assert func(5) ==  5 
